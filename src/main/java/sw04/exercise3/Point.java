b9f2b99c380f8fefbfe8da92f69fad1e1a09a348
Yannis Krämer <yannkra21@gmail.com>
2020-03-17 17:30:41 +0100

    Added sw04.exercise3

---
+++ src/main/java/sw04/exercise3/Point.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw04.exercise3;
+
+/**
+ *
+ * @author yannk
+ */
+public class Point {
+    public Point(int x, int y) {
+        
+    }
+}
