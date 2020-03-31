58b832425c238762d477b94b2a8da678660c7ec4
Yannis Krämer <yannkra21@gmail.com>
2020-03-31 11:48:53 +0200

    Added sw06 exercise1

---
+++ src/main/java/sw06/exercise1/Rectangle.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw06.exercise1;
+
+/**
+ *
+ * @author yannk
+ */
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+    
+    public Rectangle(int x, int y, int width, int height) {
+        super(x, y);
+        this.changeDimension(width, height);
+    }
+    
+    public final void changeDimension(int newWidth, int newHeight) {
+        this.width = newWidth;
+        this.height = newHeight;
+    }
+    
+    public int getWidth() {
+        return this.width;
+    }
+    
+    public int getHeight() {
+        return this.height;
+    }
+    
+    @Override
+    public int getPerimeter() {
+        return 2 * (this.getWidth() + this.getHeight());
+    }
+    
+    @Override
+    public int getArea() {
+        return this.getWidth() * this.getHeight();
+    }
+}
---
+++ src/main/java/sw06/exercise1/Shape.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw06.exercise1;
+
+/**
+ *
+ * @author yannk
+ */
+public abstract class Shape {
+    private int x;
+    private int y;
+    
+    protected Shape(int x, int y) {
+        // überschreibbare Methode in Konstruktor gefährlich
+        // deshalb "final" nützen
+        this.move(x, y);
+    }
+    
+    public final void move(int newX, int newY) {
+        this.x = newX;
+        this.y = newY;
+    }
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+    
+    // abstrakte Methoden dokumentieren
+    public abstract int getPerimeter();
+    
+    public abstract int getArea();
+}
