5db1a0522c265bee8f3e7d55381f9ee3c52c8529
Yannis Krämer <yannkra21@gmail.com>
2020-04-28 08:44:55 +0200

    Added AggregatePhase in sw10.exercise3

---
+++ src/main/java/ch/hslu/oop/sw10/exercise3/AggregatePhase.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.oop.sw10.exercise3;
+
+/**
+ *
+ * @author yannk
+ */
+public enum AggregatePhase {
+    SOLID,
+    LIQUID,
+    GAS
+}
