a9304e91e184d6fc318b6734545666f7efb7f8bb
Yannis Krämer <yannkra21@gmail.com>
2020-05-05 11:16:07 +0200

    Added missing exercises in sw11/exercise1

--- src/main/java/ch/hslu/oop/sw11/exercise1/DemoNumberInput.java
+++ src/main/java/ch/hslu/oop/sw11/exercise1/DemoNumberInput.java
@@ -6,6 +6,7 @@
 package ch.hslu.oop.sw11.exercise1;
 
 import java.util.Scanner;
+import ch.hslu.oop.sw11.exercise2.Temperature;
 
 /**
  *
@@ -13,6 +14,12 @@ import java.util.Scanner;
  */
 public class DemoNumberInput {
     public static void main(String[] args) {
+        unlimitedNumbers();
+        //getInputAndBreakAfterFirstNumber();
+        //unlimitedTemperature();
+    }
+    
+    public static void getInputAndBreakAfterFirstNumber() {
         float temperature;
         Scanner scanner = new Scanner(System.in);
         boolean inputIsNotExit;
@@ -32,6 +39,61 @@ public class DemoNumberInput {
                 }
             }
         } while (inputIsNotExit && noValidNumberWasEntered);
+        scanner.close();
+        System.out.println("program ended");
+    }
+    
+    /**
+     * Unlimited POWER!
+     */
+    public static void unlimitedNumbers() {
+        float temperature;
+        Scanner scanner = new Scanner(System.in);
+        boolean inputIsNotExit;
+        do {
+            System.out.println("Please enter temperature in °C (write 'exit' to end): ");
+            String input = scanner.next();
+            inputIsNotExit = !input.equals("exit");
+            if (inputIsNotExit) {
+                try {
+                    temperature = Float.valueOf(input);
+                    System.out.println("Temperature: " + temperature + "°C");
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Input has to be a number or 'exit'. Example: 24.5");
+                }
+            }
+        } while (inputIsNotExit);
+        scanner.close();
+        System.out.println("program ended");
+    }
+    
+    /**
+     * Even more unlimited POWER!
+     */
+    public static void unlimitedTemperature() {
+        Temperature temperature;
+        Scanner scanner = new Scanner(System.in);
+        boolean inputIsNotExit;
+        do {
+            System.out.println("Please enter temperature in °C (write 'exit' to end): ");
+            String input = scanner.next();
+            inputIsNotExit = !input.equals("exit");
+            if (inputIsNotExit) {
+                try {
+                    temperature = Temperature.createFromCelsius(Float.valueOf(input));
+                    System.out.println("Temperature: " + temperature + "°C");
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Input has to be a number or 'exit'. Example: 24.5");
+                }
+                catch (IllegalArgumentException ex) {
+                    // Temperature class threw an error
+                    System.out.println("Input has to be in range of possible.");
+                }
+            }
+        } while (inputIsNotExit);
+        scanner.close();
         System.out.println("program ended");
     }
 }
